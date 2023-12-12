3979f64bcf7c0c8877ff2495798acc04555ef2f5
Shubham <shubham.butle@gmail.com>
2023-12-12 13:04:06 +0530

    profile image issue

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -10,7 +10,7 @@ export const action =
   async ({ request }) => {
     const formData = await request.formData();
     const file = formData.get('avatar');
-    if (file && file.size > 500000) {
+    if (file && file.size > 5000000) {
       toast.error('Image size too large');
       return null;
     }
